1c516af1672504ca8c856d4b66c03ed842ffb5bc
ShemarS <42276240+ShemarS@users.noreply.github.com>
2022-06-16 12:22:05 -0400

    Rough Score Implementation
    Score floats above player

--- Shore-Buddies-PG/Assets/inventory.cs
+++ Shore-Buddies-PG/Assets/inventory.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
 
 public class inventory : MonoBehaviour
 {
+    public TMP_Text scoreText;
+    int score = 0; 
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Spawnable")) 
         {
             Collect(other.GetComponent<Collectible>());
         }
+    }
+
+    public void UpdateTextBox() {
+        scoreText.text = score.ToString();
     }
 
     private void Collect (Collectible collectible)
@@ -19,28 +27,35 @@ public class inventory : MonoBehaviour
             if(collectible is batteryCollectible)
             {
                 Debug.Log("battery collected");
+                score += 20;
             }
             else if(collectible is flipFlopCollectible)
             {
                 Debug.Log("flip flop collected");
+                score += 5;
             }
             else if(collectible is cartonCollectible)
             {
                 Debug.Log("carton collected");
+                score += 10;
             }
             else if(collectible is ringCollectible)
             {
                 Debug.Log("ring collected");
+                score += 10;
             }
-             else if(collectible is canCollectible)
+            else if(collectible is canCollectible)
             {
                 Debug.Log("can collected");
+                score += 5;
             }
             else
             {
                 Debug.Log("Nothing Collected");
             }
+            
         }
-        
+        UpdateTextBox();
     }
+
 }
